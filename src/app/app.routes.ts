5a87216931621a8329f8de371256eecc46507378
Christian de Abreu Borges <christianborges27@hotmail.com>
2025-04-10 22:00:12 -0300

    inclusao tela login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { ContainerComponent } from './components/container/container.component';
 import { HomeComponent } from './page/home/home.component';
 import { ServicoComponent } from './page/servico/servico.component';
+import { LoginComponent } from './page/login/login.component';
 
 export const routes: Routes = [
+    {
+        path: 'login', component: LoginComponent
+    },
     { path: '', component: HomeComponent },
     { path: 'contatos', component: ContainerComponent },
     { path: 'home/:id', component: HomeComponent },
     { path: 'servicos', component: ServicoComponent },
+
 ];
---
+++ src/app/page/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  email: string = '';
+  senha: string = '';
+
+  constructor(private http: HttpClient) { }
+
+
+
+  login(form: any) {
+    if (form.invalid) {
+      alert('Preencha todos os campos obrigatórios!');
+      return;
+    }
+
+    if (this.email === 'christian' && this.senha === '1234') {
+      alert('Login bem-sucedido!');
+    }
+    else {
+      alert('Credenciais inválidas. Tente novamente.');
+    }
+
+    return;
+  }
+}
